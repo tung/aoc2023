140dcc9b68268ce2cc9462193daf29d38a5ffac2
Tung Nguyen <tungtn3@gmail.com>
2023-12-20 23:09:54 +1100

    day20pt2: done
    Like day8pt2, this is another solution that doesn't solve the general
    case and makes assumptions about the nature of the input.
    
    The lead-in to the final module takes inputs from four separate networks
    that run in separate cycles, so this solution just drives each broadcast
    output individually to detect those sub-network cycles, and prints the
    cycles out so they can be plugged into an LCM calculator of your choice.
    
    Light editing of the puzzle input allows it to be fed into graphviz,
    which reveals the four sub-networks.

---
+++ day20pt2/src/main.rs
@@ -0,0 +1,189 @@
+use std::collections::VecDeque;
+
+#[derive(Debug)]
+enum ModuleType {
+    Broadcast,
+    FlipFlop(bool),
+    Conjunction(Vec<(usize, bool)>),
+}
+
+#[derive(Debug)]
+struct Module {
+    module_type: ModuleType,
+    outputs: Vec<usize>,
+}
+
+#[derive(Debug)]
+struct Message {
+    src: usize,
+    dest: usize,
+    pulse: bool,
+}
+
+struct Network {
+    modules: Vec<Module>,
+    broadcast_index: usize,
+    final_index: usize,
+}
+
+impl Network {
+    fn new(lines: &[String]) -> Self {
+        // create modules
+        let mut modules: Vec<Module> = lines
+            .iter()
+            .map(|line| Module {
+                module_type: match line.chars().next() {
+                    Some('%') => ModuleType::FlipFlop(false),
+                    Some('&') => ModuleType::Conjunction(Vec::new()),
+                    Some('b') => ModuleType::Broadcast,
+                    _ => panic!("module type"),
+                },
+                outputs: Vec::new(),
+            })
+            .collect::<Vec<Module>>();
+
+        // gather valid module names
+        let module_names = lines
+            .iter()
+            .map(|line| {
+                line.trim_start_matches(&['%', '&'])
+                    .split_once(' ')
+                    .expect("space after module name")
+                    .0
+                    .to_string()
+            })
+            .collect::<Vec<String>>();
+
+        // assign outputs
+        for (module, line) in modules.iter_mut().zip(lines.iter()) {
+            module
+                .outputs
+                .extend(
+                    line.split_once(" -> ")
+                        .expect("->")
+                        .1
+                        .split(", ")
+                        .map(|output_name| {
+                            module_names
+                                .iter()
+                                .position(|name| name == output_name)
+                                .unwrap_or(module_names.len())
+                        }),
+                );
+        }
+
+        // initialize inputs for conjunction modules
+        for i in 0..modules.len() {
+            for o in 0..modules[i].outputs.len() {
+                let dest = modules[i].outputs[o];
+                if let Some(Module {
+                    module_type: ModuleType::Conjunction(inputs),
+                    ..
+                }) = modules.get_mut(dest)
+                {
+                    inputs.push((i, false));
+                }
+            }
+        }
+
+        let broadcast_index = modules
+            .iter()
+            .position(|m| matches!(m.module_type, ModuleType::Broadcast))
+            .expect("broadcast index");
+
+        let final_index = modules
+            .iter()
+            .position(|m| m.outputs.iter().any(|&o| o == modules.len()))
+            .expect("final index");
+
+        Self {
+            modules,
+            broadcast_index,
+            final_index,
+        }
+    }
+
+    fn push_button(&mut self) -> Vec<usize> {
+        let mut cycles: Vec<usize> = Vec::new();
+
+        for broadcast_out_index in 0..self.modules[self.broadcast_index].outputs.len() {
+            let mut pushes: usize = 0;
+
+            'push_loop: loop {
+                pushes += 1;
+                let mut queue: VecDeque<Message> = VecDeque::new();
+                queue.push_back(Message {
+                    src: self.broadcast_index,
+                    dest: self.modules[self.broadcast_index].outputs[broadcast_out_index],
+                    pulse: false,
+                });
+
+                while let Some(msg) = queue.pop_front() {
+                    if msg.dest >= self.modules.len() {
+                        continue;
+                    }
+                    match &mut self.modules[msg.dest].module_type {
+                        ModuleType::Broadcast => {}
+                        ModuleType::FlipFlop(state) => {
+                            if !msg.pulse {
+                                *state = !*state;
+                            }
+                        }
+                        ModuleType::Conjunction(inputs) => {
+                            let input_memory = inputs
+                                .iter_mut()
+                                .find(|(i, _)| *i == msg.src)
+                                .expect("input memory for src in conjunction module");
+                            input_memory.1 = msg.pulse;
+                        }
+                    }
+                    if msg.pulse && msg.dest == self.final_index {
+                        cycles.push(pushes);
+                        break 'push_loop;
+                    }
+                    for o in &self.modules[msg.dest].outputs {
+                        match &self.modules[msg.dest].module_type {
+                            ModuleType::Broadcast => {
+                                queue.push_back(Message {
+                                    src: msg.dest,
+                                    dest: *o,
+                                    pulse: msg.pulse,
+                                });
+                            }
+                            ModuleType::FlipFlop(state) => {
+                                if !msg.pulse {
+                                    queue.push_back(Message {
+                                        src: msg.dest,
+                                        dest: *o,
+                                        pulse: *state,
+                                    });
+                                }
+                            }
+                            ModuleType::Conjunction(inputs) => {
+                                queue.push_back(Message {
+                                    src: msg.dest,
+                                    dest: *o,
+                                    pulse: !inputs.iter().all(|(_, m)| *m),
+                                });
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        cycles
+    }
+}
+
+fn main() {
+    let mut network = Network::new(
+        std::io::stdin()
+            .lines()
+            .map(Result::unwrap)
+            .collect::<Vec<String>>()
+            .as_slice(),
+    );
+    let cycles = network.push_button();
+    println!("least common multiple of: {cycles:?}");
+}
